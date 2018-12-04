4ca220369373b44b57635d17ffa89710be0bb0d2
mngr0 <ngrmarco@tutanota.com@>
2018-12-04 11:26:27 +0100

    removed import in init

--- rhea/cores/misc/__init__.py
+++ rhea/cores/misc/__init__.py
@@ -2,17 +2,4 @@
 from __future__ import absolute_import
 
 from .timer_ticks import glbl_timer_ticks
-from .assign import assign
 from .syncro import syncro
-
-from .serio import io_stub
-from .uix import led_count
-from .uix import led_stroby
-from .uix import led_dance
-from .uix import button_debounce
-from .uix import led_peripheral
-from .uix import button_controller
-from .uix import led_blinker
-
-
-
--- rhea/cores/spi/__init__.py
+++ rhea/cores/spi/__init__.py
@@ -2,7 +2,6 @@
 from __future__ import absolute_import
 
 from .spibus import SPIBus
-from .spi import spi_controller
-from .spi_models import spi_controller_model, SPISlave
+#from .spi_models import spi_controller_model, SPISlave
 from .spi_slave_fifo import spi_slave_fifo
 from .spi_slave_fifo_async import spi_slave_fifo_async
