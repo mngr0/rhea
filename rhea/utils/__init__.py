b011733acbf7d2a9a678011e5663e83e3dce8936
mngr0 <ngrmarco@tutanota.com@>
2018-12-04 11:34:45 +0100

    cleaned cores

--- rhea/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from __future__ import absolute_import
-
-from .command_packet import CommandPacket
-from .keep import keep_port_names
-from .extract import extract_freq
--- rhea/utils/extract.py
+++
@@ -1,8 +0,0 @@
-
-
-def extract_freq(clock, f=1):
-    if hasattr(clock, 'frequency'):
-        freq = clock.frequency
-    else:
-        freq = f
-    return freq
